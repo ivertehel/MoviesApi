9b3507b94be37d1d507aa8c62308d96664355632
Ivan Vertehel <ivan.vertehel@formpipe.com>
2023-05-24 14:12:05 +0300

    Fix build

---
+++ ApiApplication/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using ApiApplication.Components.CreateShowtime;
+using ApiApplication.Database.Entities;
+using AutoMapper;
+
+namespace ApiApplication
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<MovieEntity, CreateShowtimeCommandResult>();
+        }
+    }
+}
